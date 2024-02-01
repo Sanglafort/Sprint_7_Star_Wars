c56117a168485332d40486d190df8fde338a0038
Sanglafort <silviaangfort@gmail.com>
2024-02-01 13:41:03 +0100

    Starship list OK

---
+++ src/app/starship-list/starship-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starship-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './starship-list.component.html',
+  styleUrl: './starship-list.component.css'
+})
+export class StarshipListComponent {
+
+}
