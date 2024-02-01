8c4dc8ff79224aa472d77cd0988269a07755e07f
Sanglafort <silviaangfort@gmail.com>
2024-02-01 11:18:27 +0100

    Estructura proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '' , component: HomeComponent}, // , canActivate: [AuthGuard]},
+  { path: 'account/login' , component: LoginComponent},
+  { path: 'account/register' , component: RegisterComponent},
+  { path: '**' , redirectTo: ''},
+
+];
